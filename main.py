84f989136c176bca3fd0470f981486e6232a6aad
Vitaliy Safonov <safonov.vitvrn@gmail.com>
2020-06-24 15:57:22 +0300

    Добавил ХМЛ парсер

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import iiot_device_pb2_grpc
 from datetime import datetime
 from google.protobuf.timestamp_pb2 import Timestamp
 import mes_data as md
+from mes_data import toXML
+
 
 def proto(eui):
 	msg = iiot_device_pb2.DeviceDataRequest()
@@ -43,15 +45,20 @@ def toMesData(lastDataResponse):
 	return mesDataList
 
 
-#print(str(getDevices(getChannel()).devices[0].dev_eui))
 channel = getChannel()
 euis = getEuiList(getDevices(channel).devices)
-
 lastDataResponse = getDeviceData(euis, channel)
 mesData = toMesData(lastDataResponse)
 #Здесь парсер в xml
-for i in mesData:
-	i.show()
+toXML(mesData)
+
+#Для тестирования
+#testData = []
+#testData.append(md.MesData('1313FA534.123', '2019-04-16 14:35:00', 55))
+#testData.append(md.MesData('1313123135252FA534.321', '2017-04-16 14:35:00', 75))
+#testData.append(md.MesData('1313FA5FFFF34.123', '2018-04-16 14:35:00', 25.99))
+#toXML(testData)
+
 
 
 
--- mes_data.py
+++ mes_data.py
@@ -5,6 +5,9 @@ import iiot_device_pb2
 import iiot_device_pb2_grpc
 from datetime import datetime
 from google.protobuf.timestamp_pb2 import Timestamp
+import xml.etree.ElementTree as xml
+from xml.etree import ElementTree
+from xml.dom import minidom
 
 class MesData(object):
 	
@@ -22,4 +25,28 @@ class MesData(object):
 		print("Tag name = ", self.TAGName)
 		print("Timestamp = ", self.ts)
 		print("Value = ", self.value)
+
+def prettify(elem):
+	rough_string = ElementTree.tostring(elem, 'utf-8')
+	reparsed = minidom.parseString(rough_string)
+	return reparsed.toprettyxml(indent="  ")
+
+def toXML(mesDataObjList):
+	root = xml.Element('Import')
+	dataList = xml.Element('DataList')
+	dataList.set('Version', '1.0.71')
+	root.append(dataList)
+	for i in mesDataObjList:
+		tag = xml.SubElement(dataList, 'Tag')
+		tag.set('Name', str(i.TAGName))
+		data = xml.SubElement(tag, 'Data')
+		ts = xml.SubElement(data, 'TimeStamp')
+		ts.text = str(i.ts)
+		v = xml.SubElement(data, 'Value')
+		v.text = str(i.value)
+		dataQ = xml.SubElement(data, 'DataQuality')
+		dataQ.text = 'Good'
+	root = prettify(root)
+	file = open('test2.xml', 'w')
+	file.write(root)
 
