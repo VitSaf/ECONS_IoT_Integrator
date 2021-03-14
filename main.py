62a56362602965f89015488c1106c200b639cf0b
VitS <safonov.vitvrn@gmail.com>
2021-03-14 12:54:32 +0300

    KeyCloak auth added

--- main.py
+++ main.py
@@ -1,42 +1,32 @@
-from concurrent import futures
+﻿from concurrent import futures
 import logging
 import grpc
 import iiot_device_pb2
 import iiot_device_pb2_grpc
-from datetime import datetime
+from datetime import datetime,timedelta
 from google.protobuf.timestamp_pb2 import Timestamp
 import mes_data as md
 from mes_data import toXML
-import requests
+
 #Основной скрипт, где описаны главные функции для связи с серверами
 #Здесь же происходят вызовы этих функций( в конце)
 
 
 #Ввести логин и пароль
-LOGIN = ''
-PASSWORD = ''
-URL_DEV = 'http://core.dev002.local:8080/auth-gateway/employee/log-in'
-URL_TEST = 'http://siauth-tst.sibur.local:8080/employee/log-in'
-#URL_PROD = 'http://siauth.sibur.local:8080/employee/log-in'
-IOT_IP = '127.0.0.1:7655'
-URL_PROD = 'https://vsk.iot.sibur.local/siauth/employee/log-in'
+IOT_IP = ""
+URL_PROD_CLOAK = ""
+HOURS_TO_VRN_TIME = 3
 
+login = ""
+password = ""
 
-
-
-#функция для аутентификации в siauth
-def get_token():
-	#создаем HTTP запрос с данными аутентификации
-	post_data = '{"basic_authentication":{"login":"","password":""}}'#ввести сюда логин и пароль
-	response = requests.post(URL_PROD, data = post_data, verify = False)
-	token = response.json()['data']['authority']['token']
 
 #функция, собирающая объект аргумента для передачи по grpc (в соотвествии с proto файлом) 
 def proto(eui, token):
 	#создаем объект из автогенерированного класса
 	msg = iiot_device_pb2.DeviceDataRequest()
-	#выставляем поля объекта - токен и список устройств, которые сейчас подключены к ИоТ серверу (датчики)
 	msg.token = token
+	#выставляем поля объекта - токен и список устройств, которые сейчас подключены к ИоТ серверу (датчики)
 	for i in eui:
 		msg.devEuiList.append(i)
 	return msg
@@ -50,10 +40,9 @@ def getChannel():
 def getDevices(channel, token):
 	#по принципу протокола grpc для взаимодействия с сервером создается заглушка, относительно которой вызывает функция из прото файла
 	stub = iiot_device_pb2_grpc.IotDeviceStub(channel)
-	tkn = iiot_device_pb2.DeviceListRequest()
-	#создаем объект из автогенерированного класса и указаываем токен
-	tkn.token = token
-	return stub.getDeviceList(iiot_device_pb2.DeviceListRequest(tkn))
+	req = iiot_device_pb2.DeviceListRequest()
+	req.token = token
+	return stub.getDeviceList(req, timeout = 10)
 
 #забираем из ответа сервера только действующие eui устройств и собираем из них список
 def getEuiList(devices):
@@ -72,23 +61,31 @@ def getDeviceData(euiList, channel, token):
 #в объекты класса MesData, где каждый объект соотвествует полной записи об одном устройстве(см.описание класса)
 def toMesData(lastDataResponse):
 	mesDataList = []
-	#Циклически создаем объект, на основе которых будет генерироваться xml файл
+	#Циклически создаем объекты, на основе которых будет генерироваться xml файл
 	for i in lastDataResponse.body.devices:
 		for j in i.sensors:
 			TAGName = str(i.dev_eui) + '.' + str(j.sensor_id)
-			ts = j.ts.ToDatetime()
+			ts = j.ts.ToDatetime() + timedelta(hours = HOURS_TO_VRN_TIME)
 			value = j.value
 			mesDataList.append(md.MesData(TAGName, ts, value))
 	return mesDataList
 
+def get_access_token():
+	post_data_cloak = {'grant_type':"password", "client_id":"sibur-passport","username":login,"password":password}
+	print(post_data_cloak)
+	response = requests.post(URL_PROD_CLOAK, data = post_data_cloak, verify = False)
+	return response.json()['access_token']
 
 #Вызовы функций
 channel = getChannel()#создаем канал для подключения по grpc
-token = get_token()#получаем токен от siauth
+token = get_access_token()
 euis = getEuiList(getDevices(channel, token).devices)#получаем список всех устройств на сервере ИоТ
+print(euis)
 lastDataResponse = getDeviceData(euis, channel, token)#получаем последнюю информацию с этих устройств
 mesData = toMesData(lastDataResponse)#создаем объекты с информацией от датчиков(дата, имя, значение)
 toXML(mesData)#генерируем xml
+
+
 
 
 
--- mes_data.py
+++ mes_data.py
@@ -1,48 +1,76 @@
-from concurrent import futures
+﻿from concurrent import futures
 import logging
 import grpc
 import iiot_device_pb2
 import iiot_device_pb2_grpc
-from datetime import datetime
+from datetime import datetime, timedelta
 from google.protobuf.timestamp_pb2 import Timestamp
 import xml.etree.ElementTree as xml
 from xml.etree import ElementTree
 from xml.dom import minidom
+import configparser
+
+DAYS_WITHOUT_UPD = 7
+SAVE_XML_PATH = 'F:\\Proficy Historian Data\\ImportFiles\\Incoming\\iot_last_data.xml'
+print(DAYS_WITHOUT_UPD)
 
 #Класс для инкапсуляции данных
 class MesData(object):
-	#Конструктор класса
-	def __init__(self, TAGName, ts, v):
-		self.TAGName = TAGName
-		self.ts = ts
-		self.value = v
+    #Конструктор класса
+    def __init__(self, TAGName, ts, v):
+        self.TAGName = TAGName
+        self.ts = ts
+        self.value = v
 #преобразует xml файл к читабельному человеком виду
-#(красивая разметка)	
+#(красивая разметка)    
 def prettify(elem):
-	rough_string = ElementTree.tostring(elem, 'utf-8')
-	reparsed = minidom.parseString(rough_string)
-	return reparsed.toprettyxml(indent="  ")
+    rough_string = ElementTree.tostring(elem, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="  ")
+
+
+def find_name(iiot_name):
+    config = configparser.ConfigParser()
+    config.read('settings.ini', encoding='utf-8-sig')
+    try:
+        return config['DEFAULT'][iiot_name]
+    except KeyError:
+        return -1
+        
 
 #функция, генерирующая xml файл
 #в теле функции задается структура файла, версия и содержимое
 #Всё это определялось требованиями сервера ЭКОНС
 #т.к он читает только xml файлы строго определенной иерархии
 def toXML(mesDataObjList):
-	root = xml.Element('Import')
-	dataList = xml.Element('DataList')
-	dataList.set('Version', '1.0.71')
-	root.append(dataList)
-	for i in mesDataObjList:
-		tag = xml.SubElement(dataList, 'Tag')
-		tag.set('Name', str(i.TAGName))
-		data = xml.SubElement(tag, 'Data')
-		ts = xml.SubElement(data, 'TimeStamp')
-		ts.text = str(i.ts)
-		v = xml.SubElement(data, 'Value')
-		v.text = str(i.value)
-		dataQ = xml.SubElement(data, 'DataQuality')
-		dataQ.text = 'Good'
-	root = prettify(root)
-	file = open('test2.xml', 'w')#Прописать название файла для ЭКОНС файл ридера(да и путь)
-	file.write(root)
+    root = xml.Element('Import')
+    dataList = xml.Element('DataList')
+    dataList.set('Version', '1.0.71')
+    root.append(dataList)
+    for i in mesDataObjList:
+                if i.ts < (datetime.now() - timedelta(days = DAYS_WITHOUT_UPD)):
+                    continue
+                econs_name = find_name(str(i.TAGName))
+                if econs_name == -1:
+                    continue
+                tag = xml.SubElement(dataList, 'Tag')
+                tag.set('Name', econs_name)
+                data = xml.SubElement(tag, 'Data')
+                ts = xml.SubElement(data, 'TimeStamp')
+                ts.text = str(i.ts)
+                v = xml.SubElement(data, 'Value')
+                try:
+                    val = str(i.value).split(' ')[1]
+                    print(type(i.value), val)
+                    v.text = val
+                except:
+                    continue
+                dataQ = xml.SubElement(data, 'DataQuality')
+                dataQ.text = 'Good'
+    root = prettify(root)
+    try:
+        file = open(SAVE_XML_PATH, 'w')#Прописать название файла для ЭКОНС файл ридера(да и путь)
+    except:
+        file = open('test.xml', 'w')
+    file.write(root)
 
